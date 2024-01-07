f7efaf68e0bdf276cf93b8edd8693e5779f1989c
Christopher R Schuchardt <cschuchardt88@gmail.com>
2024-01-07 04:29:48 -0500

    update

--- scripts/build-neo-cli.sh
+++ scripts/build-neo-cli.sh
@@ -2,7 +2,7 @@
 
 # dotnet build (neo-cli/amd64)
 echo -e 'Building (neo-cli/amd64) ...';
-dotnet publish ${$GITHUB_WORKSPACE}/neo/src/Neo.CLI \
+dotnet publish ${GITHUB_WORKSPACE}/neo/src/Neo.CLI \
     --framework net7.0 \
     --configuration Release \
     --runtime linux-x64 \
@@ -23,20 +23,19 @@ mkdir -vp ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64;
 mkdir -vp ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64/DEBIAN;
 mkdir -vp ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64/srv/neo-cli/db;
 mkdir -vp ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64/usr/bin;
-cp -vp ${$GITHUB_WORKSPACE}/${GITHUB_REF_NAME}/templates/debian/neo-cli/files/amd64/control ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64/DEBIAN/;
+cp -vp ${GITHUB_WORKSPACE}/${GITHUB_REF_NAME}/templates/debian/neo-cli/files/amd64/control ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64/DEBIAN/;
 cp -vpR ${NEO_PATH}/neo-cli_amd64/* ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64/srv/neo-cli/;
-cp -vp ${$GITHUB_WORKSPACE}/${GITHUB_REF_NAME}/templates/debian/neo-cli/files/*.json ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64/srv/neo-cli/;
-ln -s ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64/usr/bin/neo-cli /srv/neo-cli/neo-cli;
+cp -vp ${GITHUB_WORKSPACE}/${GITHUB_REF_NAME}/templates/debian/neo-cli/files/*.json ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64/srv/neo-cli/;
+ln -s /srv/neo-cli/neo-cli ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64/usr/bin/neo-cli;
 chmod -R a+rw ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64/srv/neo-cli/db;
-chown -R root:root ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64/srv/neo-cli/db;
-chown -R root:root ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64/srv/neo-cli/Plugins;
+chmod -R a+rw ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64/srv/neo-cli/Plugins;
 dpkg-deb --build ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64;
 dpkg-deb --info ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64.deb;
-cp -vp ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64.deb ${$GITHUB_WORKSPACE}/apt-repo/ubuntu/pool/main/;
+cp -vp ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_amd64.deb ${GITHUB_WORKSPACE}/apt-repo/ubuntu/pool/main/;
 
 # dotnet build (neo-cli/arm64)
 echo -e 'Building (neo-cli/arm64) ...';
-dotnet publish ${$GITHUB_WORKSPACE}/neo/src/Neo.CLI \
+dotnet publish ${GITHUB_WORKSPACE}/neo/src/Neo.CLI \
     --framework net7.0 \
     --configuration Release \
     --runtime linux-arm64 \
@@ -57,13 +56,12 @@ mkdir -vp ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64;
 mkdir -vp ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64/DEBIAN;
 mkdir -vp ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64/srv/neo-cli/db;
 mkdir -vp ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64/usr/bin;
-cp -vp ${$GITHUB_WORKSPACE}/${GITHUB_REF_NAME}/templates/debian/neo-cli/files/arm64/control ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64/DEBIAN/;
+cp -vp ${GITHUB_WORKSPACE}/${GITHUB_REF_NAME}/templates/debian/neo-cli/files/arm64/control ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64/DEBIAN/;
 cp -vpR ${NEO_PATH}/neo-cli_arm64/* ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64/srv/neo-cli/;
-cp -vp ${$GITHUB_WORKSPACE}/${GITHUB_REF_NAME}/templates/debian/neo-cli/files/*.json ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64/srv/neo-cli/;
-ln -s ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64/usr/bin/neo-cli /srv/neo-cli/neo-cli;
+cp -vp ${GITHUB_WORKSPACE}/${GITHUB_REF_NAME}/templates/debian/neo-cli/files/*.json ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64/srv/neo-cli/;
+ln -s /srv/neo-cli/neo-cli ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64/usr/bin/neo-cli;
 chmod -R a+rw ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64/srv/neo-cli/db;
-chown -R root:root ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64/srv/neo-cli/db;
-chown -R root:root ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64/srv/neo-cli/Plugins;
+chmod -R a+rw ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64/srv/neo-cli/Plugins;
 dpkg-deb --build ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64;
 dpkg-deb --info ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64.deb;
-cp -vp ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64.deb ${$GITHUB_WORKSPACE}/apt-repo/ubuntu/pool/main/;
+cp -vp ${DPKGS_PATH}/neo-cli_${NEO_VERSION}-1_arm64.deb ${GITHUB_WORKSPACE}/apt-repo/ubuntu/pool/main/;
